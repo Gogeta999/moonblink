2111b8c75385128ff4da9ba17c5bfcb43f6b71a9
gus <kevinlichen1412@gmail.com>
2020-06-30 14:01:38 +0630

    error

--- lib/base_widget/thirdLogin.dart
+++ lib/base_widget/thirdLogin.dart
@@ -4,134 +4,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:moonblink/global/resources_manager.dart';
+import 'package:moonblink/view_model/login_model.dart';
+import 'package:provider/provider.dart';
 
-class ThirdLogin extends StatefulWidget {
-  @override
-  _ThirdLoginState createState() => _ThirdLoginState();
-}
-
-class _ThirdLoginState extends State<ThirdLogin> {
-  final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['profile', 'email']);
-  final FacebookLogin _facebookLogin = FacebookLogin();
-
-  GoogleSignInAccount _currentUser;
-  bool _isLoggedInFb;
-
-  Future<void> _signInWithGoogle() async {
-    GoogleSignInAccount googleUser;
-    GoogleSignInAuthentication googleAuth;
-    try {
-      googleUser = await _googleSignIn.signIn();
-      googleAuth = await googleUser.authentication;
-      setState(() {
-        _currentUser = googleUser;
-      });
-      print('accessToken: ${googleAuth.accessToken}');
-      print('idToken: ${googleAuth.idToken}');
-      print('displayName: ${googleUser.displayName}');
-      print('email: ${googleUser.email}');
-      print('id: ${googleUser.id}');
-      print('photoUrl: ${googleUser.photoUrl}');
-    } catch (error) {
-      print(error);
-    }
-  }
-
-  Future<void> _signOutGoogle() async {
-    await _googleSignIn.signOut();
-    setState(() {
-      _currentUser = null;
-    });
-    print("Sign Out");
-  }
-
-  Future<void> _signInWithFacebook() async {
-    final FacebookLoginResult result = await _facebookLogin.logIn(['email']);
-    switch (result.status) {
-      case FacebookLoginStatus.loggedIn:
-        print('case loggedIn');
-        final FacebookAccessToken accessToken = result.accessToken;
-        setState(() {
-          _isLoggedInFb = true;
-        });
-        print('token: ${accessToken.token}');
-        print('declinedPermission: ${accessToken.declinedPermissions}');
-        print('expires: ${accessToken.expires}');
-        print('isValid: ${accessToken.isValid()}');
-        print('permissions: ${accessToken.permissions}');
-        print('userId: ${accessToken.userId}');
-        final graphResponse = await Dio().get(
-            'https://graph.facebook.com/${accessToken.userId}?fields=name,first_name,last_name,email&access_token=${accessToken.token}');
-        final profile = jsonDecode(graphResponse.data);
-        print(profile);
-        break;
-      case FacebookLoginStatus.cancelledByUser:
-        print('case cancelledByUser');
-        break;
-      case FacebookLoginStatus.error:
-        print('case error');
-        break;
-    }
-  }
-
-  Future<void> _signOutFacebook() async {
-    await _facebookLogin.logOut();
-    setState(() {
-      _isLoggedInFb = false;
-    });
-    print('log out');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account) {
-      setState(() {
-        _currentUser = account;
-      });
-    });
-    try {
-      _googleSignIn.signInSilently();
-    } catch (error) {
-      print(error);
-    }
-  }
-
+class ThirdLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-
-    ///no provider widget is wrapped
-    //var model = Provider.of<LoginModel>(context);
+    //var theme = Theme.of(context);
+    var model = Provider.of<LoginModel>(context);
     return Column(
       children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              color: theme.hintColor.withAlpha(50),
-              height: 0.6,
-              width: 60,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child:
-                  Text('Third Login', style: TextStyle(color: theme.hintColor)),
-            ),
-            Container(
-              color: theme.hintColor.withAlpha(50),
-              height: 0.6,
-              width: 60,
-            ),
-          ],
-        ),
         Padding(
           padding: const EdgeInsets.only(top: 20),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               GestureDetector(
-                onTap: _signInWithGoogle,
+                onTap: () {
+                  model.login(null, null, 'google').then((value) {
+                    if (value) {
+                      Navigator.of(context).pop(true);
+                    } else {
+                      model.showErrorMessage(context);
+                    }
+                  });
+                },
                 child: Image.asset(
                   ImageHelper.wrapAssetsLogo('google_logo.png'),
                   width: 40,
@@ -139,7 +36,15 @@ class _ThirdLoginState extends State<ThirdLogin> {
                 ),
               ),
               GestureDetector(
-                onTap: _signInWithFacebook,
+                onTap: () {
+                  model.login(null, null, 'facebook').then((value) {
+                    if (value) {
+                      Navigator.of(context).pop(true);
+                    } else {
+                      model.showErrorMessage(context);
+                    }
+                  });
+                },
                 child: Image.asset(
                   ImageHelper.wrapAssetsLogo('facebook_logo.png'),
                   width: 40,
@@ -149,33 +54,7 @@ class _ThirdLoginState extends State<ThirdLogin> {
             ],
           ),
         ),
-        SizedBox(height: 20),
-        //For testing
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            FutureBuilder<bool>(
-                initialData: false,
-                future: _googleSignIn.isSignedIn(),
-                builder: (context, snapshot) {
-                  return RaisedButton(
-                    onPressed: snapshot.data && _currentUser != null ? _signOutGoogle : null,
-                    child: Text('Log Out Google'),
-                    disabledColor: Colors.grey[600],
-                  );
-                }),
-            FutureBuilder<bool>(
-                initialData: false,
-                future: _facebookLogin.isLoggedIn,
-                builder: (context, snapshot) {
-                  return RaisedButton(
-                    onPressed: snapshot.data && _isLoggedInFb ? _signOutFacebook : null,
-                    child: Text('Log Out Facebook'),
-                    disabledColor: Colors.grey[600],
-                  );
-                }),
-          ],
-        )
+        SizedBox(height: 20)
       ],
     );
   }
