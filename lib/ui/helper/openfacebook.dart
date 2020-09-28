fe8cbec29168108c4c8cf4101f416af9bbf04932
gus <kevinlichen1412@gmail.com>
2020-09-28 18:29:49 +0630

    local ad change

--- lib/ui/helper/openfacebook.dart
+++ lib/ui/helper/openfacebook.dart
@@ -19,3 +19,22 @@ void openFacebookPage() async {
     await launch(pageUrl, forceSafariVC: false);
   }
 }
+
+void openCustomerServicePage() async {
+  String customerServiceUrl;
+  if (Platform.isIOS) {
+    customerServiceUrl = 'fb://profile/103254564508101';
+  } else {
+    customerServiceUrl = 'fb://page/103254564508101';
+  }
+  const String pageUrl = 'https://www.facebook.com/Moon-Go-109953790813440/';
+  try {
+    bool nativeAppLaunch = await launch(customerServiceUrl,
+        forceSafariVC: false, universalLinksOnly: true);
+    if (!nativeAppLaunch) {
+      await launch(pageUrl, forceSafariVC: false);
+    }
+  } catch (e) {
+    await launch(pageUrl, forceSafariVC: false);
+  }
+}
