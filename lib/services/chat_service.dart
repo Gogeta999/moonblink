fa7ef67439a3008e350ed1807bb06bf437d1f8dc
gus <kevinlichen1412@gmail.com>
2020-10-12 15:49:40 +0630

    Pull from KC

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -14,9 +14,9 @@ import '../models/message.dart';
 
 //Production URL
 String proSocketurl = 'https://chat.moonblinkuniverse.com';
-String devSocketUrl = 'http://54.179.117.84/';
+String devSocketUrl = 'http://157.230.35.18:8000/';
 String now = DateTime.now().toString();
-IO.Socket socket = IO.io(proSocketurl, <String, dynamic>{
+IO.Socket socket = IO.io(devSocketUrl, <String, dynamic>{
   'transports': ['websocket'],
   'autoConnect': false,
   'timeout': 2000
