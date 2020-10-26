d3f038488f5e03d34f61f59d597f4fcffc2434dc
htetwunsan <htetwunsan2000@gmail.com>
2020-10-26 18:46:17 +0630

    Game profile price modify

--- lib/ui/pages/user/update_partner_profile_page.dart
+++ lib/ui/pages/user/update_partner_profile_page.dart
@@ -116,33 +116,50 @@ class _UpdatePartnerProfilePageState extends State<UpdatePartnerProfilePage> {
                     /// [make cover in a simple container, onpress or ontap u can use pickcoverfrom gallery directly]
                     child: Stack(
                       children: <Widget>[
-                        GestureDetector(
+                        Stack(
+                          children: [
+                            GestureDetector(
 
-                            /// [You need to put before OnTap]
-                            onTap: () {
-                              CustomBottomSheet.show(
-                                  requestType: RequestType.image,
-                                  popAfterBtnPressed: true,
-                                  buttonText: G.of(context).choose,
-                                  buildContext: context,
-                                  limit: 1,
-                                  onPressed: (File file) {
-                                    setState(() {
-                                      _cover = file;
-                                    });
-                                  },
-                                  body: G.of(context).partnercover,
-                                  willCrop: true,
-                                  compressQuality: NORMAL_COMPRESS_QUALITY);
-                            },
-                            child: AspectRatio(
-                              aspectRatio: 100 / 60,
-                              child: PartnerCoverWidget(
-                                  _cover,
-                                  partnermodel,
-                                  widget.partnerUser.prfoileFromPartner
-                                      .coverImage),
-                            )),
+                                /// [You need to put before OnTap]
+                                onTap: () {
+                                  CustomBottomSheet.show(
+                                      requestType: RequestType.image,
+                                      popAfterBtnPressed: true,
+                                      buttonText: G.of(context).choose,
+                                      buildContext: context,
+                                      limit: 1,
+                                      onPressed: (File file) {
+                                        setState(() {
+                                          _cover = file;
+                                        });
+                                      },
+                                      body: G.of(context).partnercover,
+                                      willCrop: true,
+                                      compressQuality: NORMAL_COMPRESS_QUALITY);
+                                },
+                                child: AspectRatio(
+                                  aspectRatio: 100 / 60,
+                                  child: PartnerCoverWidget(
+                                      _cover,
+                                      partnermodel,
+                                      widget.partnerUser.prfoileFromPartner
+                                          .coverImage),
+                                )),
+                            Positioned(
+                              bottom: 18,
+                              right: 18,
+                              child: Container(
+                                padding: const EdgeInsets.all(4),
+                                decoration: BoxDecoration(
+                                    color: Colors.white70,
+                                    borderRadius: BorderRadius.circular(20)
+                                ),
+                                child: Icon(Icons.camera_alt_rounded, color: Colors.black,),
+                              ),
+                            ),
+                          ],
+                        ),
+
 
                         /// [same as profile image too, if null asset local image if u can click at partnerprofilewidget then click F12 to see code template]
                         Padding(
@@ -199,8 +216,8 @@ class _UpdatePartnerProfilePageState extends State<UpdatePartnerProfilePage> {
                                         ),
                                       ),
                                       Positioned(
-                                        bottom: 12,
-                                        right: 12,
+                                        bottom: 18,
+                                        right: 18,
                                         child: Container(
                                           padding: const EdgeInsets.all(4),
                                           decoration: BoxDecoration(
