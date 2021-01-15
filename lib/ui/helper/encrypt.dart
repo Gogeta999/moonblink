ac5a29a923814838a3e8c8381b6d9368714c1f93
gus <kevinlichen1412@gmail.com>
2021-01-15 13:19:49 +0630

    Encrypt Downgrade for invitaiton code

--- lib/ui/helper/encrypt.dart
+++ lib/ui/helper/encrypt.dart
@@ -2,28 +2,28 @@ import 'package:encrypt/encrypt.dart' as encryptLib;
 import 'package:moonblink/api/moonblink_dio.dart';
 
 encrypt(int id) {
-  List confuseList = [
-    '7',
-    'm',
-    '8',
-    'o',
-    '2',
-    'o',
-    '2',
-    'n',
-    '8',
-    'g',
-    '4',
-    'o'
-  ];
-  confuseList.insert(3, id);
+  // List confuseList = [
+  //   '7',
+  //   'm',
+  //   '8',
+  //   'o',
+  //   '2',
+  //   'o',
+  //   '2',
+  //   'n',
+  //   '8',
+  //   'g',
+  //   '4',
+  //   'o'
+  // ];
+  // confuseList.insert(3, id);
   // print(id.split(''));
   // List secondList = id.split('');
   // confuseList.insert(3, secondList[0]);
   // confuseList.insert(6, secondList[1]);
   // confuseList.insert(9, secondList[2]);
 
-  final inputText = confuseList.toString();
+  final inputText = id.toString();
   final key = encryptLib.Key.fromUtf8('32lengthSecretKeyFormoongoAESsys');
   final iv = encryptLib.IV.fromUtf8('16IVforMoonGo782');
 
@@ -35,6 +35,6 @@ encrypt(int id) {
 
   if (isDev) print('Encrypted Code:' + encrypted.base64);
   if (isDev) print('Decrypted Code: ' + decrypted);
-  if (isDev) print('Compare the layer of multiple 4: ' + confuseList.toString());
+  if (isDev) print('Compare the layer of multiple 4: ' + id.toString());
   return encrypted.base64;
 }
